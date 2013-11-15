8caacffa8f9f8b193eeb389f005bb84e9aba4ec9
Thomas Ballarin <tballarin@gmail.com>
2013-11-15 14:48:08 +0100

    change generator

--- lib/generators/jezmail/install/install_generator.rb
+++ lib/generators/jezmail/install/install_generator.rb
@@ -4,10 +4,15 @@ module Jezmail
   module Generators
     class InstallGenerator < Rails::Generators::NamedBase
 	desc "This generator creates an initializer file at config/initializers"      
-
-      rake 'railties:install:migrations'
-      rake 'db:migrate'
-
+		def installBDD
+      		rake("railties:install:migrations")
+      		rake("db:migrate")
+			desc "BDD installé"
+		end
+		def configroute
+      		route('mount Blorgh::Engine, at: "/blog"')
+      		desc "Route configuré"
+		end
     end
   end
 end

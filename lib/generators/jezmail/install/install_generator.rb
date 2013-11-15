45147cbe04246f74f752cf9d5ba83f2fb6f783f8
Thomas Ballarin <tballarin@gmail.com>
2013-11-15 15:01:41 +0100

    test

--- lib/generators/jezmail/install/install_generator.rb
+++ lib/generators/jezmail/install/install_generator.rb
@@ -2,17 +2,15 @@ require 'rails'
 
 module Jezmail
   module Generators
-    class InstallGenerator < Rails::Generators::Base
-	desc "This generator install jezmail in http:yourdomaine.com/webmail"      
+    class InstallGenerator < Rails::Generators::Base     
 		def copy_migrations
       		rake("railties:install:migrations")
       		rake("db:migrate")
-			desc "BDD installé"
 		end
 		def copy_route
       		route('mount Jezmail::Engine, at: "/webmail"')
-      		desc "Route configuré"
 		end
+		desc "This generator install jezmail in http:yourdomaine.com/webmail" 
     end
   end
 end
